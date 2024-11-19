3652c803bfb8a1d1e1e001dd1aff975bf811b494
Kimmm-c <97310663+Kimmm-c@users.noreply.github.com>
2024-11-18 19:31:16 -0800

    Added shot, boss, and minion scenes

---
+++ shot.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+@export var shooting_power = 0.5
+@export var shooting_angle = 30
---
+++ sprite_2d.gd
@@ -0,0 +1,7 @@
+extends Sprite2D
+
+@export var minions_per_spawn = 3
+@export var health = 25000
+
+func spawn_minions():
+	pass
