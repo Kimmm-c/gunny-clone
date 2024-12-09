83b6750283bf5675c75e6e996b70126c7a6d3968
Kimmm-c <97310663+Kimmm-c@users.noreply.github.com>
2024-12-09 11:22:12 -0800

    set up multiplayer game server and client connection

--- main.gd
+++ main.gd
@@ -2,6 +2,64 @@ extends Node
 
 @export var singleplayer_game_scene: PackedScene
 @export var multiplayer_game_scene: PackedScene
+@export var address = "127.0.0.1"
+var peer: ENetMultiplayerPeer
+var port = 9999
+var max_clients = 2 # 2 players
+
+
+func _ready() -> void:
+	multiplayer.peer_connected.connect(peer_connected)
+	multiplayer.peer_disconnected.connect(peer_disconnected)
+	multiplayer.connected_to_server.connect(connected_to_server)
+	multiplayer.connection_failed.connect(connection_failed)
+
+
+func peer_connected(id: int):
+	print("player connected. id: ", id)
+
+
+func peer_disconnected(id: int):
+	print("player disconnected. id: ", id)
+
+
+func connected_to_server():
+	print("successfully connected to the server")
+
+
+func connection_failed():
+	print("connection failed")
+
+
+func _on_host_pressed() -> void:
+	peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(port, max_clients)
+	if error != OK:
+		print("hosting failed. error: ", error)
+		return
+	multiplayer.multiplayer_peer = peer
+	print("waiting for players...")
+
+
+func _on_join_pressed() -> void:
+	peer = ENetMultiplayerPeer.new()
+	var error = peer.create_client(address, port)
+	if error != OK:
+		print("creating client failed. error: ", error)
+		return
+	multiplayer.multiplayer_peer = peer
+
+
+func _on_start_game_pressed() -> void:
+	start_multiplayer_game.rpc()
+
+
+@rpc("any_peer", "call_local")
+func start_multiplayer_game():
+	$UI.hide()
+	var multiplayer_game = multiplayer_game_scene.instantiate()
+	add_child(multiplayer_game)
+	multiplayer_game.game_end.connect(_on_game_end.bind())
 
 
 func _on_singleplayer_mode_btn_pressed() -> void:
