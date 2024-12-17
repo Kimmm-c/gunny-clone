7132fc938ef860f98f2e7c4f30c4431128aefdc7
Kimmm-c <97310663+Kimmm-c@users.noreply.github.com>
2024-12-16 22:03:49 -0800

    continue with multiplayer game controller implementation

--- MultiplayerGameController/multiplayer_game_controller.gd
+++ MultiplayerGameController/multiplayer_game_controller.gd
@@ -2,14 +2,34 @@ extends Node2D
 
 @export var player_scene: PackedScene
 
+enum PlayerTurn {
+	PLAYER1,
+	PLAYER2
+}
+
+var player_turn = PlayerTurn.PLAYER1
+
 func _ready() -> void:
 	var index = 0
+	# spawn players
 	for i in GameManager.Players:
 		var locations = get_tree().get_nodes_in_group("PlayerSpawnLocations")
 		for loc in locations:
 			if loc.name == str(index): 
 				var player = player_scene.instantiate()
+				player.name = str(GameManager.Players[i].id)
 				player.position = loc.global_position
 				add_child(player)
 		index += 1
 		
+	# spawn shot for the corresponding instance
+	# this shot is controllable by a specific player
+	
+	
+	# spawn the wind. This wind's properties should be applied to the shot 
+	# of the designated player.
+	
+
+
+func _on_turn_timer_timeout() -> void:
+	pass # Replace with function body.
--- Player/player.gd
+++ Player/player.gd
@@ -11,6 +11,7 @@ var is_listening = true
 var prev_position: Vector2
 var state: CharacterState
 var idle_direction: Vector2
+
 @export var health = MAX_HEALTH:
 	set(value):
 		var damage = health - value
@@ -28,35 +29,37 @@ func _ready() -> void:
 	prev_position = position
 	state = CharacterState.IDLE
 	idle_direction = Vector2(1, 0)
+	$MultiplayerSynchronizer.set_multiplayer_authority(name.to_int())
 
 
 func _physics_process(delta: float) -> void:
-	if position != prev_position:
-		change_position.emit(position)
-		prev_position = position
-	
-	velocity.y += GRAVITY * delta
-	if is_listening:
-		var direction = Vector2.ZERO
-		if Input.is_action_pressed("player_moves_left"):
-			direction.x -= 1
-		elif Input.is_action_pressed("player_moves_right"):
-			direction.x += 1
-	
-		velocity.x = direction.x * speed
+	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+		if position != prev_position:
+			change_position.emit(position)
+			prev_position = position
 		
-		if direction != idle_direction and direction != Vector2.ZERO:
-			change_direction.emit()
+		velocity.y += GRAVITY * delta
+		if is_listening:
+			var direction = Vector2.ZERO
+			if Input.is_action_pressed("player_moves_left"):
+				direction.x -= 1
+			elif Input.is_action_pressed("player_moves_right"):
+				direction.x += 1
+		
+			velocity.x = direction.x * speed
 			
-		if direction != Vector2.ZERO:
-			idle_direction = direction
-			state = CharacterState.MOVING
-		else:
-			state = CharacterState.IDLE
-		
-	move_and_slide()
-	play_animation()
-	play_SFX()
+			if direction != idle_direction and direction != Vector2.ZERO:
+				change_direction.emit()
+				
+			if direction != Vector2.ZERO:
+				idle_direction = direction
+				state = CharacterState.MOVING
+			else:
+				state = CharacterState.IDLE
+			
+		move_and_slide()
+		play_animation()
+		play_SFX()
 
 
 func attack() -> void:
